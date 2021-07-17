5cad3beea15139c2d37586b048245d947a43b90a
safkat33 <safkat.33@gmail.com>
2021-07-17 13:10:34 +0900

    [main]  add comment

--- MergeSort/code.py
+++ MergeSort/code.py
@@ -13,26 +13,30 @@ def merge_sort(arr):
         merge_sort(right_array)
 
         # merge
-        left_array_pointer = 0
-        right_array_pointer = 0
+        i = 0   # left arr pointer
+        j = 0   # left arr pointer
         merged_array_index = 0
-        while left_array_pointer < len(left_array) and right_array_pointer < len(right_array):
-            if left_array[left_array_pointer] < right_array[right_array_pointer]:
-                arr[merged_array_index] = left_array[left_array_pointer]
-                left_array_pointer += 1
+
+        # compare between the left array and right array
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                arr[merged_array_index] = left_array[i]
+                i += 1
             else:
-                arr[merged_array_index] = right_array[right_array_pointer]
-                right_array_pointer += 1
+                arr[merged_array_index] = right_array[j]
+                j += 1
             merged_array_index += 1
 
-        while left_array_pointer < len(left_array):
-            arr[merged_array_index] = left_array[left_array_pointer]
-            left_array_pointer += 1
+        # when right array completed traversing but left array element exists
+        while i < len(left_array):
+            arr[merged_array_index] = left_array[i]
+            i += 1
             merged_array_index += 1
 
-        while right_array_pointer < len(right_array):
-            arr[merged_array_index] = right_array[right_array_pointer]
-            right_array_pointer += 1
+        # when left array completed traversing but right array element exists
+        while j < len(right_array):
+            arr[merged_array_index] = right_array[j]
+            j += 1
             merged_array_index += 1
 
     return arr
