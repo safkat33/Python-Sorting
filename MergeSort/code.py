4b7d786e4936383999dc34689650f2bbd9ddb049
safkat.arefin <safkat.arefin@co-well.jp>
2021-07-17 11:07:50 +0900

    init

---
+++ MergeSort/code.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+
+def merge_sort(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    merge_sort('PyCharm')
+
