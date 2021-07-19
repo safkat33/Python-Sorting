2f1a7480b4655c3c19268be751f784d2c3cc6256
safkat33 <safkat.33@gmail.com>
2021-07-19 17:39:00 +0900

    updated the Quick Sort

--- QuickSort/code.py
+++ QuickSort/code.py
@@ -1,40 +1,48 @@
 # This is a sample Python script.
 
-def swap(arr, a, b):
-    temp = arr[b]
-    arr[b] = arr[a]
-    arr[a] = temp
-    return arr
+# def swap(arr, a, b):
+#     temp = arr[b]
+#     arr[b] = arr[a]
+#     arr[a] = temp
+#     return arr
 
 
-def partition(arr, l, r):
-    pivot_index = r
-    i = l
-    j = l
-    while j < r:
+def partition(arr, left, right):
+    pivot_index = right
+    i = left
+    j = left
+    while j < right:
         if arr[j] < arr[pivot_index]:
-            swap(arr, i, j)
+            # swap(arr, i, j)
+            arr[i], arr[j] = arr[j], arr[i]
             i += 1
         j += 1
 
-    swap(arr, i, pivot_index)
+    # swap(arr, i, pivot_index)
+    arr[i], arr[pivot_index] = arr[pivot_index], arr[i]
     pivot_index = i
     return pivot_index
 
 
-def quick_sort(arr, l, r):
+def quick_sort(arr, left, right):
     # Use a breakpoint in the code line below to debug your script.
-    if l >= r:
+    if left >= right:
         return arr
 
-    pivot = partition(arr, l, r)
+    pivot = partition(arr, left, right)
 
-    quick_sort(arr, l, pivot-1)
-    quick_sort(arr, pivot+1, r)
+    quick_sort(arr, left, pivot - 1)
+    quick_sort(arr, pivot + 1, right)
 
     return arr
 
 
 arr_test = [2, 1, 6, 8, 9, 11, 7, 5, 3, 4, 10]
 
-print(quick_sort(arr_test, 0, len(arr_test) - 1))
+
+class SortArray:
+    def __init__(self, array_test):
+        self.arr_test = array_test
+    print("Un-sorted Array", arr_test)
+    sorted_array = quick_sort(arr_test, 0, len(arr_test) - 1)
+    print("Sorted Array", sorted_array)
